4fb53dd547683d0539c76da1e85e8daf5a9e1420
Flozyo <160837581+Flozyoo@users.noreply.github.com>
2025-12-21 15:08:56 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInAnonymously, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getFirestore, collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc, query, onSnapshot, increment, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCRDoOpYiyTHURxLhcq9adRX9__djYj_J0",
+    authDomain: "news-d6614.firebaseapp.com",
+    projectId: "news-d6614",
+    storageBucket: "news-d6614.firebasestorage.app",
+    messagingSenderId: "818748521624",
+    appId: "1:818748521624:web:4afd501bc7991464d724ed",
+    measurementId: "G-TG0CW4PZXF"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const appId = "news-d6614-main";
+
+export const initAuth = async () => {
+    if (!auth.currentUser) return await signInAnonymously(auth);
+    return auth.currentUser;
+};
+
+export { 
+    auth, db, appId, onAuthStateChanged, collection, doc, getDoc, getDocs, 
+    addDoc, updateDoc, deleteDoc, query, onSnapshot, increment, setDoc, 
+    serverTimestamp, signInWithEmailAndPassword, signOut 
+};
